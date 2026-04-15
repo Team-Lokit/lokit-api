2a0d6a333003093afe8272f6e0ee0ad9d26d255e
songhaechan <bukak2019@naver.com>
2026-04-15 21:55:10 +0900

    feat: application-dev.yaml에 애플 환경변수 추가 및 apple prperties.kt 수

--- src/main/kotlin/kr/co/lokit/api/domain/user/infrastructure/oauth/apple/AppleOAuthProperties.kt
+++ src/main/kotlin/kr/co/lokit/api/domain/user/infrastructure/oauth/apple/AppleOAuthProperties.kt
@@ -5,12 +5,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "apple")
 data class AppleOAuthProperties(
     val clientId: String,
-    val clientSecret: String,
+    val teamId: String,
+    val keyId: String,
+    val privateKey: String,
     val redirectUri: String,
     val frontRedirectUri: String,
 ) {
     companion object {
         const val AUTHORIZATION_URL = "https://appleid.apple.com/auth/authorize"
         const val TOKEN_URL = "https://appleid.apple.com/auth/token"
+        const val PUBLIC_KEYS_URL = "https://appleid.apple.com/auth/keys"
     }
 }
