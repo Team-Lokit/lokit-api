36854bf5fca248ca91a5408f1b4f85534352f089
songhaechan <bukak2019@naver.com>
2026-04-20 18:00:25 +0900

    refactor: OAuthServiceRegistryPort를 인터페이스를 두고 설계하지않고 바로 OAuthServiceRegistry를 통해 호출하는 것으로 변경
    - Port라는 명칭이 맞는 것 같지도 않고 과도한 인터페이스 도출같아 수

--- src/main/kotlin/kr/co/lokit/api/domain/user/application/OAuthLoginServiceRegistry.kt
+++ src/main/kotlin/kr/co/lokit/api/domain/user/application/OAuthLoginServiceRegistry.kt
@@ -1,17 +1,16 @@
 package kr.co.lokit.api.domain.user.application
 
 import kr.co.lokit.api.domain.user.application.port.OAuthProvider
-import kr.co.lokit.api.domain.user.application.port.OAuthServiceRegistryPort
 import org.springframework.stereotype.Component
 
 @Component
 class OAuthLoginServiceRegistry(
     services: List<LoginService>,
-) : OAuthServiceRegistryPort {
+) {
     private val clientMap: Map<OAuthProvider, LoginService> =
         services.associateBy { it.provider }
 
-    override fun getService(provider: OAuthProvider): LoginService =
+    fun getService(provider: OAuthProvider): LoginService =
         clientMap[provider]
             ?: throw IllegalArgumentException("지원하지 않는 OAuth 서비스입니다: $provider")
 }
--- src/main/kotlin/kr/co/lokit/api/domain/user/application/port/OAuthServiceRegistryPort.kt
+++
@@ -1,7 +0,0 @@
-package kr.co.lokit.api.domain.user.application.port
-
-import kr.co.lokit.api.domain.user.application.LoginService
-
-interface OAuthServiceRegistryPort {
-    fun getService(provider: OAuthProvider): LoginService
-}
